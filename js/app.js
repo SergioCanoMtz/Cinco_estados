659d4a1eb19cdd90a6daa7ed01652b9dc22097f9
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-04-08 15:39:06 -0500

    Funcionamiento al 10%

--- js/app.js
+++ js/app.js
@@ -1,6 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){  
+    
+    var nombre = document.getElementById('nombre');
+    var btnCreate = document.getElementById('btnCreate');
+    var id = 0;
+    //var bgBlanco = document.getElementById('bg-white');
 
-function nuevo(){
-    var nombre = document.getElementById('nombre').value;
+    nombre.addEventListener('keyup', () => {
+        document.getElementById('nuevo').innerHTML = '<div id="bg-white">'+nombre.value+'</div>';
 
-    document.getElementById('nuevo').innerHTML = nombre;
-}
+        var bgBlanco = document.getElementById('bg-white');
+
+        if(bgBlanco.value != ''){
+            bgBlanco.style.background = 'white';
+            bgBlanco.style.color = 'black';
+            bgBlanco.style.display = 'inline';
+            bgBlanco.style.padding = '3px';
+            bgBlanco.style.borderRadius = '2px';
+            bgBlanco.style.fontFamily = 'Arial';
+        } else{
+            bgBlanco.style.display = 'none';
+        }
+    });
+
+    btnCreate.addEventListener('click', ()=>{
+
+        if(nombre.value == '' || nombre.value == null){
+            alert('Ingresa el nombre de un Proceso');
+        } else{
+            /* document.getElementById('bg-white').style.transform = 'translate(100px,100px)';
+            document.getElementById('bg-white').style.transitionDuration = '1s'; */
+            /* var proceso = document.createElement('p');
+            var contenido = document.createTextNode(document.getElementById('bg-white').value);
+            proceso.appendChild(contenido);
+
+            proceso.setAttribute('class','caja');
+
+            var contenedor = document.getElementById('listo');
+            contenedor.appendChild(proceso); */
+
+        }
+    })
+    
+});
