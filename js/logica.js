06a546fbfd266d1f6b78916458fbeebf31009133
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-04-13 17:14:46 -0500

    Falta solo unirlo

--- js/logica.js
+++ js/logica.js
@@ -1,6 +1,9 @@
-    var num = 1;
-    var id = 1;
-    var p = "proceso";
+    var num = 1,
+        id = 1,
+        ms = 3000,
+        numProcesos = 4,
+        p = "proceso";
+        
     var nuevo = [],
             listo = [],
             ejecutando = [],
@@ -17,13 +20,22 @@
     }
 
     function inicio(){
-        while(num <= 3){
+        while(num <= numProcesos){
             nuevo.push(p+num);
             num++;
         }    
     }
 
     function segundo(){
+
+        function sleep(milliseconds) {
+            var start = new Date().getTime();
+            for (var i = 0; i < 1e7; i++) {
+                if ((new Date().getTime() - start) > milliseconds) {
+                    break;
+                }
+            }
+        }
 
         listo = nuevo.slice();
 
@@ -35,29 +47,34 @@
         }
 
         while(ejecutando.length > 0){
+            sleep(2000);
             var numRandom = getRandomInt(3,6); //4
-
             if(numRandom === 3){
                 listo.push(ejecutando[0]);
                 console.log(ejecutando[0]+' pasa de ejecutando a listo '+(id++));
+                sleep(2000);
                 /* console.log(ejecutando[0]+' Se elimina de ejecutando '+(id++)) */
                 ejecutando.shift(0);
                 ejecutando.push(listo[0]);
                 console.log(listo[0]+' pasa de listo a ejecutando '+(id++));
+                sleep(2000);
                 /* console.log(listo[0]+' se elimina de listo '+(id++)) */
                 listo.shift(0);
             } else if(numRandom === 4){
                 bloqueado.push(ejecutando[0]);
                 console.log(ejecutando[0]+' pasa de ejecutando a bloqueado '+(id++));
+                sleep(2000);
                 /* console.log(ejecutando[0]+' Se elimina de ejecutando '+(id++)) */
                 ejecutando.shift(0);
                 listo.push(bloqueado[0]);
                 console.log(bloqueado[0]+' pasa de bloqueado a listo '+(id++));
+                sleep(2000);
                 /* console.log(bloqueado[0]+' se elimina de bloqueado '+(id++)); */
                 bloqueado.shift(0);
                 if(listo.length > 0 ){
                     ejecutando.push(listo[0]);
                     console.log(listo[0]+' pasa de listo a ejecutando '+(id++));
+                    sleep(2000);
                     /* console.log(listo[0]+' se elimina de listo '+(id++)); */
                     listo.shift(0);
                 }
@@ -65,15 +82,17 @@
             } else if(numRandom === 5){
                 saliente.push(ejecutando[0]);
                 console.log(ejecutando[0]+' pasa de ejecutando a saliente '+(id++));
+                sleep(2000);
                 /* console.log(ejecutando[0]+' Se elimina de ejecutando '+(id++)); */
                 ejecutando.shift(0);
                 if(listo.length > 0){
                     ejecutando.push(listo[0]);
                     console.log(listo[0]+' pasa de listo a ejecutando '+(id++));
+                    sleep(2000);
                     /* console.log(listo[0]+' se elimina de listo '+(id++)); */
                     listo.shift(0);
                 } 
-            }
+            }       
         }        
     }
 /* 
